9fba8a143c430ca6fa712dbc5003159668c5cc9f
kero0000 <chengmunchew@gmail.com>
2021-09-30 11:35:24 +0800

    Merge remote-tracking branch 'origin/androidapp' into androidapp

---
+++ app/src/main/java/com/example/authapp3/DirectionFinderListener.java
@@ -0,0 +1,11 @@
+package com.example.authapp3;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface DirectionFinderListener {
+    void onDirectionFinderStart();
+    void onDirectionFinderSuccess(List<Route> route);
+}
