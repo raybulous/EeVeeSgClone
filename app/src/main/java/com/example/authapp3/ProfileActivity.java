db0c95fa2d710b194076346865be4b9f7a231c7b
Gabriel <glow015@e.ntu.edu.sg>
2021-09-30 23:24:16 +0800

    UI changes

--- app/src/main/java/com/example/authapp3/ProfileActivity.java
+++ app/src/main/java/com/example/authapp3/ProfileActivity.java
@@ -1,23 +1,19 @@
 package com.example.authapp3;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.ActivityOptions;
 import android.content.Intent;
-import android.transition.Explode;
+import android.os.Bundle;
 import android.transition.Fade;
-import android.transition.Slide;
 import android.transition.Transition;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -43,21 +39,12 @@ public class ProfileActivity extends AppCompatActivity {
         ev = (Button) findViewById(R.id.evButton);
         qr = (Button) findViewById(R.id.qrButton);
         profile = (Button) findViewById(R.id.profileButton);
-        navigate = (Button) findViewById(R.id.Nav);
         logout = (Button) findViewById(R.id.signOut);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 FirebaseAuth.getInstance().signOut();
                 startActivity(new Intent(ProfileActivity.this, MainActivity.class));
-            }
-        });
-
-        // button to change to Navigate page
-        navigate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ProfileActivity.this, Navigate.class));
             }
         });
 
