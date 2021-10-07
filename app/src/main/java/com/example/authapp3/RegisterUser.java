15849710efd78c2101b3a05779959fbf051bc2cc
Gabriel <glow015@e.ntu.edu.sg>
2021-10-07 21:43:45 +0800

    Code optimisations

--- app/src/main/java/com/example/authapp3/RegisterUser.java
+++ app/src/main/java/com/example/authapp3/RegisterUser.java
@@ -2,7 +2,6 @@ package com.example.authapp3;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ProgressBar;
