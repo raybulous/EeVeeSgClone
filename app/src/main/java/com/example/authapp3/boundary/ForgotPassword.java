e97228d5a7cb8240de824a7208f64f9422f1f3cb
Gabriel <glow015@e.ntu.edu.sg>
2021-10-12 22:29:02 +0800

    Register and Forgot Password UI update

--- app/src/main/java/com/example/authapp3/boundary/ForgotPassword.java
+++ app/src/main/java/com/example/authapp3/boundary/ForgotPassword.java
@@ -1,19 +1,17 @@
 package com.example.authapp3.boundary;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.authapp3.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.example.authapp3.control.Profile;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPassword extends AppCompatActivity {
@@ -34,39 +32,27 @@ public class ForgotPassword extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
 
-        resetPasswordButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                resetPassword();
-            }
-        });
+        resetPasswordButton.setOnClickListener(view -> resetPassword());
     }
 
     private void resetPassword(){
         String email = emailEditText.getText().toString().trim();
 
-        if(email.isEmpty()){
-            emailEditText.setError("Please enter your email!");
+        String emailCheck = Profile.checkEmail(email);
+        if(!emailCheck.equals("No Error")){
+            emailEditText.setError(emailCheck);
             emailEditText.requestFocus();
             return;
         }
 
-        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-            emailEditText.setError("Please enter a valid email!");
-            emailEditText.requestFocus();
-            return;
-        }
         progressBar.setVisibility(View.GONE);
-        auth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(ForgotPassword.this, "Check your email to reset your password",Toast.LENGTH_LONG).show();
-                    ForgotPassword.this.finish();
-                }
-                else{
-                    Toast.makeText(ForgotPassword.this, "Try again! Something wrong happened", Toast.LENGTH_LONG).show();
-                }
+        auth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+            if(task.isSuccessful()){
+                Toast.makeText(ForgotPassword.this, "Check your email to reset your password",Toast.LENGTH_LONG).show();
+                ForgotPassword.this.finish();
+            }
+            else{
+                Toast.makeText(ForgotPassword.this, "Try again! Something wrong happened", Toast.LENGTH_LONG).show();
             }
         });
     }
